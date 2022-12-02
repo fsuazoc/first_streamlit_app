443f4250b39a8f3a6b32a9e177092a9afe0a8e10
Francisco <119678869+fsuazoc@users.noreply.github.com>
2022-12-02 16:04:34 -0300

    Update streamlit_app.py
    agregando filtro a las frutas.

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #Pongamos una lista de selección aquí para que puedan escoger la fruta que quieren incluir
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 
 #Mostrar la tabla en la página.
 streamlit.dataframe(my_fruit_list)
