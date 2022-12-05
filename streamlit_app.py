99b1dd600fa5add52786b446152e788b5ba467e2
Francisco <119678869+fsuazoc@users.noreply.github.com>
2022-12-05 14:52:57 -0300

    Update streamlit_app.py
    Colocando la lista de frutas seleccionadas, en una variable.

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,8 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #Pongamos una lista de selección aquí para que puedan escoger la fruta que quieren incluir
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 #Mostrar la tabla en la página.
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
