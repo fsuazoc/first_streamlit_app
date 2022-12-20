a4a8bc2b006fe446d95fdc7e36570ecaefe45ae8
Francisco <119678869+fsuazoc@users.noreply.github.com>
2022-12-20 13:03:24 -0300

    Update streamlit_app.py
    se agrega boton para poder ver la lista de frutas, mediante una function.

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,21 +41,24 @@ try:
     back_from_function = get_fruityvice_data (fruit_choice)
     #Mostrarlo en la pantalla como una tabla
     streamlit.dataframe (back_from_function)
-    
 except URLError as e:
     streamlit.error()
 
+streamlit.header("The Fruit load list contains:")
+#Snowflake-related functions
+def get_fruit_load_list():
+    with my_cnx.cursor() to my_cur:
+        my_cur.execute("SELECT * FROM fruit_load_list")
+        return my_cur.fetchall()
+
+#Add a button to load the fruit
+if streamlit.button('Get Fruit Load List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fruit_load_list()
+    streamlit.dataframe(my_data_rows)
+    
 #detenemos esta parte para que no se ejecute.
 streamlit.stop()
-
-#import snowflake.connector
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * FROM fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The Fruit load list contains:")
-streamlit.dataframe(my_data_rows)
 
 # Permitir que el usuario final agregue una fruta a la lista
 add_my_fruit = streamlit.text_input ('What fruit would you like to add?', 'jackfruit')
